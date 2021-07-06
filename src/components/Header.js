3db8109e0ff7878ec876883887615c2972419da9
DerpHead <alexraia4@gmail.com>
2021-07-05 22:01:24 -0400

    most boilerplate

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+      return (
+            <div className="Header">
+                  <p>Header Comp</p>
+            </div>
+      );
+}
+
+export default Header;
