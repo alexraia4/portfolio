785fd93cc6c4378698d1607e6af2ed7686382aee
DerpHead <alexraia4@gmail.com>
2021-07-08 23:17:10 -0400

    derp

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import '../css/Home.css'
 function Home() {
       return (
             <div className="Home">
-                  <p>I'm A recent graduate of Dev Mountain and looking for a career in software development. I created this portfolio site to showcase what i've learned through my degree in Computer Information Technology and a 3 month coding bootcamp. I've come to "develop" (pun intended) a strong passion for software principles and I can't wait to show the world what I can do!</p>
+                  <p>I'm A recent graduate of Dev Mountain and looking for a career in software development. I created this portfolio site to showcase what i've learned throughout my degree in Computer Information Technology and a 3 month coding bootcamp. I've come to "develop" (pun intended) a strong passion for software principles and I can't wait to show the world what I can do!</p>
             </div>
       );
 }
