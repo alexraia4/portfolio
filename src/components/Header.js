95f0637b0d10a648bf9adfdb189b84e8324eef62
DerpHead <alexraia4@gmail.com>
2021-07-14 22:16:06 -0400

    ...

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ import '../css/Header.css'
 function Header() {
       return (
             <header className="Header">
-                  <div className="Header_pic"></div>
-                  <Link to="/" className="Header_title"><h1>Alexander Raia</h1></Link>
+                  <div className="Header_me">
+                        <div className="Header_pic"/>
+                        <Link to="/" className="Header_title"><h1>Alexander Raia</h1></Link>
+                  </div>
                   <nav className="Header_links">
                         <Link to="/about" className="Header_link"><p>About</p></Link>
                         <Link to="/projects" className="Header_link"><p>Projects</p></Link>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@ import '../css/Home.css'
 function Home() {
       return (
             <div className="Home">
-                  <p>I'm A recent graduate of Dev Mountain and looking for a career in software development. I created this portfolio site to showcase what i've learned throughout my degree in Computer Information Technology and a 3 month coding bootcamp. I've come to "develop" (pun intended) a strong passion for software principles and I can't wait to show the world what I can do!</p>
+                  <h1 className="Home_title">Excellence Through Software</h1>
+                  <p>I'm A recent graduate of Dev Mountain and looking for a career in software development. I created this portfolio site to showcase what I've learned throughout my degree in Computer Information Technology and a 3 month coding bootcamp. I've come to "develop" (pun intended) a strong passion for software principles and I can't wait to show the world what I can do!</p>
             </div>
       );
 }
