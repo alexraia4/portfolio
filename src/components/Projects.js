efd5a2a4fc63165adbedd60e420f90c9feeacfe8
DerpHead <alexraia4@gmail.com>
2021-07-21 14:53:36 -0400

    fixed project link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ function Projects() {
       return (
             <div className="Projects">
                   <div className = "Projects_timeline">
-                        <a href="https://www.w3schools.com" className = "Projects_timeline_link"/>
+                        <a href="http://134.209.72.40:3003/" className = "Projects_timeline_link"/>
                         <div className = "Projects_timeline_content">
                               <p className = "Projects_timeline_title">Timeline Generator</p>
                               <p className = "Projects_timeline_context">For this project, I set out to create an app that lets users store historical data into timelines. This app lets user pick a length of time and it automatically generates the years. Users are then able to add events to each year which can be very helpful to track anything from important dates in history to a log of one's personel life moments.</p>
