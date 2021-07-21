6d2e156fcadaa4dd6ad4d409038ca82e2c1de891
DerpHead <alexraia4@gmail.com>
2021-07-21 14:25:25 -0400

    finished contact page for now

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,14 @@ import '../css/Contact.css'
 function Contact() {
       return (
             <div className="Contact">
-                  <p>Phone: (516) 780-2348</p>
-                  <p>Email: alexraia4@gmail.com</p>
+                  <p className = "Contact_phone">Phone:</p>
+                  <p className = "Contact_number">(516) 780-2348</p>
+                  <p className = "Contact_email">Email:</p>
+                  <p className = "Contact_gmail">alexraia4@gmail.com</p>
+                  <a href="https://www.linkedin.com/in/alexander-raia/" className = "Contact_linkedin_link"/>
+                  <a href="https://www.github.com/alexraia4" className = "Contact_github_link"/>
+                  
+                  
             </div>
       );
 }
