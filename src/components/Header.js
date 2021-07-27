6d7e7deeb52f507c2f581f172aae7838c02badda
DerpHead <alexraia4@gmail.com>
2021-07-27 16:15:39 -0400

    ...

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
                         <Link to="/" className="Header_title"><h1>Alexander Raia</h1></Link>
                   </div>
                   <nav className="Header_links">
-                        <a href="https://drive.google.com/file/d/1es-JPaKmpCtUOamFH2i_WZ9qQnVax0lZ/view?usp=sharing" className="Header_link"><p>Resume</p></a>
+                        <a href="https://drive.google.com/file/d/1UPRmW_4NQaI0qmzxyY8sd09hc6ee7rdt/view?usp=sharing" className="Header_link"><p>Résumé</p></a>
                         <Link to="/projects" className="Header_link"><p>Projects</p></Link>
                         <Link to="/contact" className="Header_link"><p>Contact</p></Link>
                   </nav>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import Project from '../components/Project'
 function Projects() {
       return (
             <div className="Projects">
-                  <Project link = "http://134.209.72.40:300" title = "Timeline Generator" context = "I created an app that lets users store historical data into timelines. Users pick a length of time and it automatically generates the years into a timeline format. Users then add events to each year to track anything from important dates in history to a log of one's personel life moments. Have fun creating multiple timelines!"/>
+                  <Project link = "http://134.209.72.40:3003" title = "Timeline Generator" context = "I created an app that lets users store historical data into timelines. Users pick a length of time and it automatically generates the years into a timeline format. Users then add events to each year to track anything from important dates in history to a log of one's personel life moments. Have fun creating multiple timelines!"/>
             </div>
       );
 }
