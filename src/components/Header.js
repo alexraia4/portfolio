2b5bc5240317bb3b9c374c1cef010a7ff133a247
DerpHead <alexraia4@gmail.com>
2021-07-26 23:03:23 -0400

    fixed resume link

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
                         <Link to="/" className="Header_title"><h1>Alexander Raia</h1></Link>
                   </div>
                   <nav className="Header_links">
-                        <Link to="/about" className="Header_link"><p>Resume</p></Link>
+                        <a href="https://drive.google.com/file/d/1es-JPaKmpCtUOamFH2i_WZ9qQnVax0lZ/view?usp=sharing" className="Header_link"><p>Resume</p></a>
                         <Link to="/projects" className="Header_link"><p>Projects</p></Link>
                         <Link to="/contact" className="Header_link"><p>Contact</p></Link>
                   </nav>
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Home     from './components/Home'
-import About    from './components/About'
 import Projects from './components/Projects'
 import Contact  from './components/Contact'
 
 export default (
       <Switch>
             <Route component={Home} exact path="/" />
-            <Route component={About} path="/about" />
             <Route component={Projects} path="/projects" />
             <Route component={Contact} path="/contact" />
       </Switch>
