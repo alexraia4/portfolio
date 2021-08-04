1e1d79992c270e9ef67804510bed34601e7aba71
DerpHead <alexraia4@gmail.com>
2021-08-04 00:45:50 -0400

    ...

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
                         <Link to="/" className="Header_title"><h1>Alexander Raia</h1></Link>
                   </div>
                   <nav className="Header_links">
-                        <a href="https://drive.google.com/file/d/1UPRmW_4NQaI0qmzxyY8sd09hc6ee7rdt/view?usp=sharing" className="Header_link"><p>Résumé</p></a>
+                        <a href="https://docs.google.com/document/d/1o2CK0zm48t1kXBYujEKx1a52jLxJ2-0F2iRdn_M3xkY/edit?usp=sharing" className="Header_link"><p>Resumé</p></a>
                         <Link to="/projects" className="Header_link"><p>Projects</p></Link>
                         <Link to="/contact" className="Header_link"><p>Contact</p></Link>
                   </nav>
