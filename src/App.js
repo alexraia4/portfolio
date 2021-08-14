abfcaa1c0688ab094472553d24639bfb98070a06
DerpHead <alexraia4@gmail.com>
2021-08-13 21:22:42 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import routes from './routes'
 import Header from './components/Header'
+import Home     from './components/Home'
+import Projects from './components/Projects'
+import Contact  from './components/Contact'
 import './css/App.css'
 
 function App() {
       return (
             <div className="App">
                   <Header/>
-                  
+                  <Home/>
+                  <Projects/>
+                  <Contact/>
             </div>
       );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,11 @@ import '../css/Header.css'
 function Header() {
       return (
             <header className="Header">
-                  <div className="Header_me">
-                        <div className="Header_pic"/>
-                        <Link to="/" className="Header_title"><h1>Alexander Raia</h1></Link>
-                  </div>
-                  <nav className="Header_links">
-                        <a href="https://docs.google.com/document/d/1o2CK0zm48t1kXBYujEKx1a52jLxJ2-0F2iRdn_M3xkY/edit?usp=sharing" className="Header_link"><p>Resumé</p></a>
-                        <Link to="/projects" className="Header_link"><p>Projects</p></Link>
-                        <Link to="/contact" className="Header_link"><p>Contact</p></Link>
-                  </nav>
+                  <p>Home</p>
+                  <p>About</p>
+                  <p>Technologies</p>
+                  <p>Projects</p>
+                  <p>Contact</p>
             </header>
       );
 }
