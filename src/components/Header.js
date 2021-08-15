b087c31cd48b070ca73f71c24e0220d3f26bdb6f
DerpHead <alexraia4@gmail.com>
2021-08-14 21:40:25 -0400

    ...

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,36 @@
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import hamburger from '../images/hamburger.png'
 import '../css/Header.css'
 
 function Header() {
+      const [mobileMenuView, setmMV] = useState(false);
+
+      const changeView = () => {
+            setmMV(!mobileMenuView)
+      }
+
+      const checkView = () => {
+            if (mobileMenuView === false) {
+                  console.log("derp")
+            }
+      }
+
+
       return (
             <header className="Header">
-                  <p>Home</p>
-                  <p>About</p>
-                  <p>Technologies</p>
-                  <p>Projects</p>
-                  <p>Contact</p>
+                  
+                  <nav className = "Header_nav">
+                        <p>Home</p>
+                        <p>About</p>
+                        <p>Technologies</p>
+                        <p>Projects</p>
+                        <p>Contact</p>
+                  </nav>
+
+                  <img src={hamburger} onClick={changeView}/>
+
+                  {checkView()}
+
             </header>
       );
 }
